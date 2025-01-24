64c8cf57fbacf5ac0e47cbb48630baac40b15b15
Stefano Sorrentino <32730851+SleepingSteven@users.noreply.github.com>
2025-01-24 15:59:52 +0100

    Update llms_and_embeddings.py

--- app/llms_and_embeddings.py
+++ app/llms_and_embeddings.py
@@ -3,8 +3,11 @@ from langsmith.wrappers import wrap_openai
 from langchain_huggingface import HuggingFaceEmbeddings
 import openai
 import config_setup
+import os
 
-llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.6)
+model=os.getenv("MODEL_NAME")
+
+llm = ChatOpenAI(model_name=model, temperature=0.6)
 client = wrap_openai(openai.Client())
 
 embeddings_model_name = "sentence-transformers/all-MiniLM-L6-v2"
